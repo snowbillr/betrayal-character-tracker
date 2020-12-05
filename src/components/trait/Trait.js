21b9ee781ebf8fac9544a3330e13920d686b8991
Bill Reed <billnreed@gmail.com>
2020-12-05 14:15:29 -0500

    cleanup and mobile styles

--- src/components/trait/Trait.js
+++ src/components/trait/Trait.js
@@ -1,18 +1,10 @@
 import "./Trait.scss";
 
-import { useState } from "react";
 import { TraitSlider } from '../trait-slider/TraitSlider';
 
 export function Trait({ name }) {
-  const [value, setValue] = useState(5);
-
   return (
     <div className="trait">
-      {/* <button className="trait-button decrement" onClick={() => setValue(value - 1)}>-</button>
-      <div className="value">
-        <div className="value-text">{value}</div>
-      </div>
-      <button className="trait-button increment" onClick={() => setValue(value + 1)}>+</button> */}
       <TraitSlider />
       <h2 className="name">{name}</h2>
     </div>
